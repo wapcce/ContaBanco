bfbe3c3d2d8e11581442641b3406a4b7332cbc4a
wagner.prado <wapce@bol.com.br>
2023-09-09 10:05:48 -0300

    Conclusão do exercicio

--- src/Cliente.java
+++ src/Cliente.java
@@ -1,10 +1,30 @@
 import java.util.Scanner;
+
 public class Cliente {
     int numero;
     String agencia;
     String nomeCliente;
     float saldo;
 
+    Scanner sc = new Scanner(System.in);
     
+    public void dadosCliente(){
+        System.out.println("Por favor, digite o número da Agência! ");
+        agencia = sc.nextLine();
+        System.out.println("Por favor, digite seu nome! ");
+        nomeCliente = sc.nextLine();
+        System.out.println("Por favor, digite sua conta! ");
+        numero = sc.nextInt();
+        System.out.println("Por favor, digite seu Saldo! ");
+        saldo = sc.nextFloat();
+
+        System.out.println("Olá "+ nomeCliente +", obrigado por criar uma conta em nosso banco," + 
+        "sua agência é "+ agencia + ", conta "+ numero +" e seu saldo "+ saldo +" já está disponível para saque.");
+
+        
+
+
+
+    }
     
 }
